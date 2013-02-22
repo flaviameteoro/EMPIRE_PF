e018aa4fc17af5cfbcebe2489c922ac464b9060a
Philip Browne <p.browne@reading.ac.uk>
2013-02-22 15:16:14 +0000

    adding changes for printing debug statements

--- pf_couple.f90
+++ pf_couple.f90
@@ -10,7 +10,8 @@ program couple_pf
   
   integer :: i,j,particle
   integer :: mpi_err
-  
+  write(6,'(A)') 'PF: Starting PF code'
+  call flush(6)
   call initialise_mpi
   
 !  call allocate_data
@@ -20,21 +21,26 @@ program couple_pf
   call set_pf_controls
 
   call allocate_pf
-  
+  write(6,*) 'PF: starting to recieve from model'
 ! 1st call to model to get psi
   do particle =1,pf%ngrand
      call recieve_from_model(pf%psi(:,particle),particle)
   enddo
-
+  write(6,*) 'PF: All models recieved in pf couple' 
+  call flush(6)
   do j=1,pf%time_obs
-
+     write(6,*) 'PF: j = ',j
      do i = 1,pf%time_bwn_obs-1
         pf%timestep = pf%timestep + 1
         call proposal_filter
+        write(6,*) 'PF: i = ',i, 'after proposal filter'
+        call flush(6)
      end do
            
      pf%timestep = pf%timestep + 1
      call equal_weight_filter
+     write(6,*) 'PF: timestep = ',pf%timestep, 'after equal weight filter'
+     call flush(6)
 
   enddo
 
