99e1e7907799b4319f4dd01adadafadf05bc2df1
Philip Browne <p.browne@reading.ac.uk>
2013-04-15 13:58:56 +0100

    updated pf_couple to include outputting and ensemble generation

--- pf_couple.f90
+++ pf_couple.f90
@@ -10,6 +10,7 @@ program couple_pf
   
   integer :: i,j,particle
   integer :: mpi_err
+  real(kind=kind(1.0D0)),dimension(3) :: rdom
   write(6,'(A)') 'PF: Starting PF code'
   call flush(6)
   call initialise_mpi
@@ -25,9 +26,17 @@ program couple_pf
 ! 1st call to model to get psi
   do particle =1,pf%ngrand
      call recieve_from_model(pf%psi(:,particle),particle)
+
+     !lets add some random noise to the initial conditions
+     call NormalRandomNumbers1D(0.0D0,sqrt(2.0D0),3,rdom)
+     pf%psi(:,particle) = pf%psi(:,particle) + rdom
+
   enddo
   write(6,*) 'PF: All models recieved in pf couple' 
   call flush(6)
+  if(pf%gen_data) call save_truth(pf%psi(:,1))
+  call output_from_pf
+
   do j=1,pf%time_obs
      write(6,*) 'PF: j = ',j
      do i = 1,pf%time_bwn_obs-1
@@ -35,12 +44,16 @@ program couple_pf
         call proposal_filter
         write(6,*) 'PF: i = ',i, 'after proposal filter'
         call flush(6)
+        
+        call output_from_pf
      end do
            
      pf%timestep = pf%timestep + 1
      call equal_weight_filter
      write(6,*) 'PF: timestep = ',pf%timestep, 'after equal weight filter'
      call flush(6)
+
+     call output_from_pf
 
   enddo
   write(6,*) 'PF: finished the loop - now to tidy up'
